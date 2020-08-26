23acd9da3a3fb17dcf9ca149ef91d3c836625d16
Deepika <bhuvanadeepika017@gmail.com>
2020-08-26 17:04:11 +0530

    added build

---
+++ build/precache-manifest.1345571671288ba0980b228dd641201c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38fabab148f85f5c48f8fd3521ada697",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8c7ce51ffdadd98b2f50",
+    "url": "/static/css/main.a1eedd89.chunk.css"
+  },
+  {
+    "revision": "a37461c8ef0bf750d97c",
+    "url": "/static/js/2.18be2ce6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.18be2ce6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c7ce51ffdadd98b2f50",
+    "url": "/static/js/main.14d7e700.chunk.js"
+  },
+  {
+    "revision": "6a52c016a8220507e85f",
+    "url": "/static/js/runtime-main.f1e5c95c.js"
+  }
+]);
