2bea053c9d4b38001f938d2151d3e6c93410ad28
Deepika <bhuvanadeepika017@gmail.com>
2020-08-28 11:43:03 +0530

    added getWinnerStatus in game class

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,8 +1,18 @@
 import React from 'react';
-import { calculateWinner } from '../calculateWinner';
 import Button from './Button';
 import Board from './Board';
 import Status from './Status';
+
+const WINNING_CONDITIONS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+];
 
 class Game extends React.Component {
   constructor(props) {
@@ -18,6 +28,13 @@ class Game extends React.Component {
     this.restartGame = this.restartGame.bind(this);
   }
 
+  getWinnerStatus(board) {
+    const { currentPlayer } = this.state;
+    const doesInclude = (index) => board[index] === currentPlayer.symbol;
+    const isWin = WINNING_CONDITIONS.some((row) => row.every(doesInclude));
+    return isWin ? currentPlayer : null;
+  }
+
   handleChange(id) {
     const board = this.state.board.slice();
     board[id] = this.state.currentPlayer.symbol;
@@ -27,7 +44,7 @@ class Game extends React.Component {
       currentPlayer: nextPlayer,
       nextPlayer: currentPlayer,
       isDraw: board.every((value) => value),
-      winner: calculateWinner(board, currentPlayer)
+      winner: this.getWinnerStatus(board)
     }));
   };
 
