39ff8a3b6be23f3dc1e6bc8544fd442d908487ca
Deepika <bhuvanadeepika017@gmail.com>
2020-08-28 11:53:19 +0530

    Updates

---
+++ precache-manifest.23195ac0f8d8076f37fffbaa47c489ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a8b416225731371954d2b646087dd07",
+    "url": "/ticTacToeWithJsx/index.html"
+  },
+  {
+    "revision": "e174305f6b380d2d044b",
+    "url": "/ticTacToeWithJsx/static/css/main.ff7361fa.chunk.css"
+  },
+  {
+    "revision": "5ab36d9cf32e10fb8526",
+    "url": "/ticTacToeWithJsx/static/js/2.7a48256b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ticTacToeWithJsx/static/js/2.7a48256b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e174305f6b380d2d044b",
+    "url": "/ticTacToeWithJsx/static/js/main.38746344.chunk.js"
+  },
+  {
+    "revision": "8104364ec6a512f8b6b6",
+    "url": "/ticTacToeWithJsx/static/js/runtime-main.79a1f9fb.js"
+  }
+]);
